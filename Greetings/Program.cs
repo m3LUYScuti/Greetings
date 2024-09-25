4dfd302a66fb9a57bdb1481b16c74e3f86fa8e9f
davit <davit@GOSH>
2024-09-25 10:09:12 +0400

    Add project files.

---
+++ Greetings/Program.cs
@@ -0,0 +1,45 @@
+﻿//DateTime TarigiSaati = DateTime.Now;
+     Console.WriteLine("Sheiyvanet Saati");
+
+     TimeSpan Dro = TimeSpan.Parse(Console.ReadLine());
+
+   //TimeSpan Dro = TarigiSaati.TimeOfDay; 
+
+       
+   TimeSpan Dila = new TimeSpan(5, 0, 0);  
+   TimeSpan DilisDasasruli = new TimeSpan(11, 59, 59);  
+
+   TimeSpan ShuaDge = new TimeSpan(12, 0, 0); 
+   TimeSpan ShuaDgisDasasruli = new TimeSpan(16, 59, 59); 
+
+   TimeSpan Sagamo = new TimeSpan(17, 0, 0);   
+   TimeSpan SagamosDasasruli = new TimeSpan(20, 59, 59);   
+
+   TimeSpan Game = new TimeSpan(21, 0, 0);     
+   TimeSpan GamisDasasruli = new TimeSpan(4, 59, 59);      
+
+        
+        if (Dro >= Dila && Dro <= DilisDasasruli)
+        {
+            Console.WriteLine("Good Morning");
+        }
+        else if (Dro >= ShuaDge && Dro <= ShuaDgisDasasruli)
+        {
+            Console.WriteLine("Good Afternoon");
+        }
+        else if (Dro >= Sagamo && Dro <= SagamosDasasruli)
+        {
+            Console.WriteLine("Good Evening");
+        }
+        else if (Dro >= Game || Dro <= GamisDasasruli)
+        {
+            Console.WriteLine("Good Night");
+        }
+        else
+        {
+            Console.WriteLine("Araswori Dro.");
+        }
+
+        Console.ReadLine(); 
+    
+
